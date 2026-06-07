44b3b07fe3703518b4213ad4cdb100067ed77461
CamCraig91 <cam.arn.craig@gmail.com>
2026-06-07 16:55:40 -0400

    Create oauth-callback.js

---
+++ api/oauth-callback.js
@@ -0,0 +1,8 @@
+import { handleCallback } from "../google.js";
+
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    return handleCallback(req, res);
+  }
+  return res.status(405).json({ error: "Method not allowed" });
+}
