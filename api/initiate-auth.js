58ab3ebb68be09793c89d13dabe8d2ba98d7cf07
CamCraig91 <cam.arn.craig@gmail.com>
2026-06-07 16:55:21 -0400

    Add initiate-auth handler for POST requests

---
+++ api/initiate-auth.js
@@ -0,0 +1,8 @@
+import { initiateAuth } from "../google.js";
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    return initiateAuth(req, res);
+  }
+  return res.status(405).json({ error: "Method not allowed" });
+}
